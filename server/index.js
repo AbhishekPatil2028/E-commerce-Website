b2de09ea9f69bc7b97e6ef8e722b66244f470012
Abhishek Patil <abhiapatil2028@gmail.com>
2026-04-21 20:19:50 +0530

    fix: add frontend url in cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 
 
 app.use(cors({
-    origin:['http://localhost:5173',],
+    origin:['http://localhost:5173',
+        "https://e-commerce-website-k49v.onrender.com"
+    ],
     credentials:true
 }))
 app.use(express.json());
