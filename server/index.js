17f4de4425e728b4b3a687b343b148dcbcd76dcc
Abhishek Patil <abhiapatil2028@gmail.com>
2026-04-21 13:41:19 +0530

    fix: updated backend for render deployment

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import cors from 'cors';
 dotenv.config();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 
 app.use(cors({
-    origin:'http://localhost:5173',
-    Credential:true
+    origin:['http://localhost:5173',],
+    credential:true
 }))
 app.use(express.json());
 
